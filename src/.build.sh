fd65e0cde60f7be99cea5b2fd71edc95f02d59e4
Witut2005 <wiktor.glogowski@onet.pl>
2022-05-29 13:09:57 +0200

    compile script update

--- src/.build.sh
+++ src/.build.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
+
 #UBUNTU TEST
 #add 32-bit gcc cross-compiler to your $PATH variable
 #ASSEMBLER COMPILATION
+
 nasm -fbin ./boot/boot.asm 
 nasm -fbin ./boot/locateFS.asm
 nasm -fbin ./boot/kernelLoader.asm
@@ -43,50 +46,27 @@ nasm -f bin syscall_test.asm -o syscall_test
 nasm -f bin xanin_apps_space.asm -o xanin_apps_space
 cd ..
 
+
 #COMPILE																										 HERE YOU MUST PUT PATH TO SRC DIRECTORY
+#alias xanincpp='i386-elf-g++ -O0 -masm=intel -Wno-builtin-declaration-mismatch -nostdlib -ffreestanding -Wno-unused-function -fno-rtti -I /home/witut/Desktop/xaninOS/src -c'
 
+shopt -s expand_aliases
+alias xanincpp='i386-elf-g++ -O0 -masm=intel -Wno-builtin-declaration-mismatch -nostdlib -ffreestanding -Wno-unused-function -Wno-write-strings -Wno-return-type -fno-rtti -I /home/witut/Desktop/xaninOS/src -c'
 
-#i386-elf-gcc -O0 -masm=intel -Wno-builtin-declaration-mismatch -nostdlib -ffreestanding -Wno-unused-function -I /home/witut/Desktop/xaninOS/src -c ./real_mode_manager/real_mode.cpp -o ./real_mode_manager/real_mode.o
-
-
-#i386-elf-g++ -O0 -masm=intel -Wno-builtin-declaration-mismatch -nostdlib -ffreestanding -Wno-unused-function -fno-rtti -I /home/witut/Desktop/xaninOS/src \
-#-c ./game_engine/xagame.cpp  -o ./game_engine/xagame.o
-
-#i386-elf-g++ -O0 -masm=intel -Wno-builtin-declaration-mismatch -nostdlib -ffreestanding -Wno-unused-function -fno-rtti -I /home/witut/Desktop/xaninOS/src \
-#-c ./xagames/xagame_test.cpp  -o ./xagames/xagame_test.o
-
-#i386-elf-g++ -std=c++2a -O0 -masm=intel -w -Wno-builtin-declaration-mismatch -Wwrite-strings -nostdlib -ffreestanding -Wno-unused-function -fno-rtti -I /home/witut/Desktop/xaninOS/src \
-#-c ./assembler/assembler.cpp  -o ./assembler/assembler.o
-
-i386-elf-g++ -O0 -masm=intel -Wno-builtin-declaration-mismatch -nostdlib -ffreestanding -Wno-unused-function -fno-rtti -I /home/witut/Desktop/xaninOS/src \
--c ./chip8/chip8.cpp  -o ./chip8/chip8.o
-O
-i386-elf-g++ -O0 -masm=intel -Wno-builtin-declaration-mismatch -nostdlib -ffreestanding -Wno-unused-function -fno-rtti -I /home/witut/Desktop/xaninOS/src \
--c ./devices/APIC/apic.cpp  -o ./devices/APIC/apic.o
-
-i386-elf-g++ -O0 -masm=intel -Wno-builtin-declaration-mismatch -Wno-write-strings -Wno-return-type -nostdlib -ffreestanding -Wno-unused-function -fno-rtti -I /home/witut/Desktop/xaninOS/src \
--c ./test/cpp_test.cpp  -o ./test/cpp_test.o
-
-#i386-elf-g++ -O0 -masm=intel -Wno-builtin-declaration-mismatch -Wno-write-strings -Wno-return-type -nostdlib -ffreestanding -Wno-unused-function -fno-rtti -I /home/witut/Desktop/xaninOS/src \
-#-c ./devices/mouse.cpp  -o ./devices/mouse.o ./devices/mouse.o
-
+xanincpp ./chip8/chip8.cpp  -o ./chip8/chip8.o
+xanincpp ./devices/APIC/apic.cpp  -o ./devices/APIC/apic.o
+xanincpp ./test/cpp_test.cpp  -o ./test/cpp_test.o
 
 i386-elf-gcc -O0 -masm=intel -Wno-discarded-qualifiers -Wno-builtin-declaration-mismatch -nostdlib -ffreestanding -Wno-int-conversion -Wno-unused-function -Wno-div-by-zero -I /home/witut/Desktop/xaninOS/src \
 kernel.c ./handlers/keyboard.o ./pit/pit.o ./syscall/syscall.o ./test/cpp_test.o ./devices/MOUSE/mouse.o ./devices/APIC/apic.o -o kernel.bin
-
-
 
 cat ./xaninApps/shutdown ./xaninApps/reboot ./xaninApps/syscall_test ./devices/HARD_DISK/load_kernel > ./xaninApps/xanin_external_apps
 
 dd if=./xaninApps/xanin_external_apps of=./xaninApps/xanin_apps_space bs=512 count=16 conv=notrunc
 
 cat ./boot/boot ./lib/enter_real_mode ./xaninApps/xanin_apps_space ./xin_fs/xin_pointers ./xin_fs/entries_table ./boot/kernelLoader kernel.bin > xanin.bin
-#        0                   1                   2                   12                          16              17                                  
-
 
 #MOV XANIN.img to BIN
 dd if=xanin.bin of=xanin.img
 mv xanin.img -f ../bin
 mv xanin.bin -f ../bin
-#rm xanin.bin 
-#rm kernel.bin
