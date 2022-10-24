173fc99d198e41983a9d093988bc042a79324e64
Witut2005 <wiktor.glogowski@onet.pl>
2022-10-24 20:16:30 +0200

    xpaint update

--- src/xin_fs/xin_xpaint.c
+++ src/xin_fs/xin_xpaint.c
@@ -5,8 +5,83 @@
 #include <libc/memory.h>
 #include <libc/string.h>
 
+uint16_t saved_cell;
+uint16_t* cursor;
 
-static uint16_t* cursor;
+void xpaint_input(void)
+{
+    
+    if(!KeyInfo.scan_code)
+        return;
+    
+    if(KeyInfo.scan_code >= 128)
+        return;
+
+    *cursor = saved_cell;
+
+    if(KeyInfo.scan_code == ARROW_RIGHT)
+    {
+        cursor++;
+    }
+
+    else if(KeyInfo.scan_code == ARROW_LEFT)
+    {
+        cursor--;
+    }
+
+    else if(KeyInfo.scan_code == ARROW_DOWN)
+    {
+        cursor = cursor + 80;
+    }
+
+    else if(KeyInfo.scan_code == ARROW_UP)
+    {
+        cursor = cursor - 80;
+    }
+
+    if(KeyInfo.is_ctrl)
+    {
+        if(KeyInfo.character)
+        {
+            uint8_t color = KeyInfo.character;
+
+            if(color >= 'a' && color <= 'f')
+                color = color - 'a' + 0xa;   
+            else if(color >= '0' && color <= '9')
+                color = color - '0';
+            else 
+                return;
+
+            color = color << 4;
+            *cursor = (uint16_t)(color << 8 | (*cursor & 0xFF));
+        }
+    }
+    
+    else
+    {
+        if(KeyInfo.character)
+        {
+        
+            uint8_t color;
+        
+            if(!(*cursor && 0xFF00))
+                color = set_output_color(black, white);
+            else
+                color = (*cursor & 0xFF00) >> 8;
+            
+            *cursor = (uint16_t)(color << 8 | KeyInfo.character);
+        }
+    }
+    
+
+    saved_cell = *cursor;
+
+    if(!KeyInfo.character)
+        *cursor = (set_output_color(black, white) << 8) | '_';
+    msleep(50);
+
+
+}
 
 
 int xin_xpaint(char* file_name)
@@ -33,10 +108,14 @@ int xin_xpaint(char* file_name)
         for(int i = 0; i < VGA_SCREEN_RESOLUTION * 2; i++)
             screen_cell[i] = (uint16_t) (data_pointer[i]);
 
-        while(KeyInfo.scan_code != ENTER);
+        cursor = (uint16_t*)VGA_TEXT_MEMORY;
+        saved_cell = *cursor;
+
+        while(KeyInfo.scan_code != F4_KEY)
+            xpaint_input();
 
     }
-
+    write(xin_file, (void*)VGA_TEXT_MEMORY, VGA_SCREEN_RESOLUTION);
     fclose(&xin_file);
     return XANIN_OK;
 
