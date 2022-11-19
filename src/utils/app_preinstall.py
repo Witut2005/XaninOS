628ec9ea79d1fc2bff1099b6ae86fab2e3e52c4a
Witut2005 <wiktor.glogowski@onet.pl>
2022-11-20 00:38:06 +0100

    preinstall update

--- src/utils/app_preinstall.py
+++ src/utils/app_preinstall.py
@@ -2,23 +2,29 @@
 import numpy as np
 from detect_xin_ext import *
 import argparse
+from colorama import Fore 
+from colorama import Style
+import sys
 
 XIN_ENTRY_SIZE = 64
 XIN_ALLOCATED = b'\x01'
 XIN_EOF = b'\xff'
 XIN_FILE = 'F'
 XIN_MAX_PERMISSIONS = b'\xFF'
+FAIL = '\033[91m'
 
 args = argparse.ArgumentParser()
 args.add_argument('-image', action='store', type=str, required=True)
 args.add_argument('-files', action='store', type=str, required=True, nargs='+')
+args.add_argument('-errors', action='store_true')
 args = args.parse_args()
+
 
 xin_extended_table = detect_xin_ext(args.image)
 print(hex(xin_extended_table))
 file = open(str(args.image), 'rb+')
 xin_filesystem_pointers_begin = int(SECTOR_SIZE * 18)
-xin_filesystem_pointers = int(SECTOR_SIZE * 18 + 0xB00)
+xin_filesystem_pointers = int(SECTOR_SIZE * 18 + 0x100)
 xin_filesystem_entries = int(SECTOR_SIZE * 26 + 0xB40 + (64 * 7))
 # file.seek(SECTOR_SIZE * 18 + 0xB00)
 
@@ -26,7 +32,16 @@ print('\n\n\n--------------------------------')
 print('XIN FILESYSTEM PREINSTALL PHARSE')
 
 for current_file in args.files:
-    aha = open(current_file, 'rb')
+    try: 
+        aha = open(current_file, 'rb')
+    except FileNotFoundError:
+        print(f'{Fore.RED}File not found:{Style.RESET_ALL} {current_file}')
+        if args.errors:
+            sys.exit()
+        print('Skipping...')
+        continue
+    print(f'{Fore.GREEN}File OK: {current_file}{Style.RESET_ALL}')
+        
     data = aha.read()
     
     file.seek(xin_filesystem_pointers)
@@ -45,11 +60,12 @@ for current_file in args.files:
     
     file.seek(xin_filesystem_entries)
     file.write(bytes('/' + current_file, 'ascii'))
-    file.seek(xin_filesystem_entries + 40)
+    file.seek(xin_filesystem_entries + 38)
     file.write(bytes(XIN_FILE, 'ascii'))
     file.write(bytes(13))
     file.write(file_lenght.to_bytes(4, 'little'))
     file.write((xin_filesystem_pointers - xin_filesystem_pointers_begin - file_lenght).to_bytes(4, 'little'))
+    # file.write(0.to_bytes(4, 'little'))
     xin_filesystem_entries += XIN_ENTRY_SIZE
     
     file.seek(SECTOR_SIZE * (xin_filesystem_pointers - xin_filesystem_pointers_begin - file_lenght))
