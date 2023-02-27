aee17cfb3b433d07097b928ee17ff2710b34e227
Witut2005 <wiktor.glogowski@onet.pl>
2023-02-27 22:39:03 +0100

    udapte

--- src/libcpp/type_traits.h
+++ src/libcpp/type_traits.h
@@ -11,4 +11,33 @@ template< class T > struct remove_pointer<T* const>          {typedef T type;};
 template< class T > struct remove_pointer<T* volatile>       {typedef T type;};
 template< class T > struct remove_pointer<T* const volatile> {typedef T type;};
 
+template<class T, T v>
+struct integral_constant
+{
+    static constexpr T value = v;
+    using value_type = T;
+    using type = integral_constant; // using injected-class-name
+    constexpr operator value_type() const noexcept { return value; }
+    constexpr value_type operator()() const noexcept { return value; } // since c++14
+};
+
+using true_type = std::integral_constant<bool, true>;
+using false_type = std::integral_constant<bool, false>;
+
+template<class T>
+struct is_pointer : std::false_type {};
+ 
+template<class T>
+struct is_pointer<T*> : std::true_type {};
+ 
+template<class T>
+struct is_pointer<T* const> : std::true_type {};
+ 
+template<class T>
+struct is_pointer<T* volatile> : std::true_type {};
+ 
+template<class T>
+struct is_pointer<T* const volatile> : std::true_type {};
+
+
 }
