9d3f1a2f6aed29fa1e3f9aa52217d65f05f1f01b
Witut2005 <wiktor.glogowski@onet.pl>
2023-07-28 00:10:06 +0200

    build cross compiler script

---
+++ src/scripts/build-cross.sh
@@ -0,0 +1,45 @@
+#ORGINAL FILE  https://github.com/mell-o-tron/MellOs/blob/main/A_Setup/setup-gcc-arch.sh
+
+#Installing dependencies
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install python3 pip3
+sudo apt-get install curl nasm qemu qemu-system-x86 virt-manager virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat base-devel bison flex libgmp-dev libmpc-dev libmpfr-dev texinfo
+
+#----------------------------------------------------------------
+#Actual building
+
+export PREFIX="/usr/local/i386elfgcc"
+export TARGET=i386-elf
+export PATH="$PREFIX/bin:$PATH"
+
+mkdir /tmp/src
+cd /tmp/src
+curl -O http://ftp.gnu.org/gnu/binutils/binutils-2.39.tar.gz
+tar xf binutils-2.39.tar.gz
+mkdir binutils-build
+cd binutils-build
+../binutils-2.39/configure --target=$TARGET --enable-interwork --enable-multilib --disable-nls --disable-werror --prefix=$PREFIX 2>&1 | tee configure.log
+sudo make all install 2>&1 | tee make.log
+
+cd /tmp/src
+curl -O https://ftp.gnu.org/gnu/gcc/gcc-12.2.0/gcc-12.2.0.tar.gz
+tar xf gcc-12.2.0.tar.gz
+mkdir gcc-build
+cd gcc-build
+echo Configure: . . . . . . .
+../gcc-12.2.0/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --disable-libssp --enable-language=c,c++ --without-headers
+echo MAKE ALL-GCC:
+sudo make all-gcc
+echo MAKE ALL-TARGET-LIBGCC:
+sudo make all-target-libgcc
+echo MAKE INSTALL-GCC:
+sudo make install-gcc
+echo MAKE INSTALL-TARGET-LIBGCC:
+sudo make install-target-libgcc
+echo HERE U GO MAYBE:
+ls /usr/local/i386elfgcc/bin
+export PATH="$PATH:/usr/local/i386elfgcc/bin"
+
+echo 'export PATH="/usr/local/i386elfgcc/bin:$PATH"' >> ~/.bashrc
