7c75793d126b12ddcda11ea49d3e39839df89d15
Witut2005 <wiktor.glogowski@onet.pl>
2023-11-12 18:13:18 +0100

    input.asm file with input syscalls

--- src/sys/call/xanin_sys/calls/input/input.asm
+++ src/sys/call/xanin_sys/calls/input/input.asm
@@ -5,24 +5,68 @@
 global __sys_getchar, __sys_getscan, __sys_inputg, __sys_getxchar, __sys_keyinfo_get
 
 __sys_getchar:              ;char getchar(void)
-mov eax, 201                ;getchar syscall id
+mov eax, XANIN_GETCHAR                ;getchar syscall id
 XANIN_INVOKE_SYSTEM_CALL
 ret                         ;eax holds return value
 
 __sys_getscan:              ;char getscan(void)
-mov eax, 202                ;getchar syscall id
+mov eax, XANIN_GETSCAN                ;getchar syscall id
 XANIN_INVOKE_SYSTEM_CALL
 ret                         ;eax holds return value
 
 __sys_getxchar:
 __sys_inputg:               ;xchar __sys_inputg(void)
-mov eax, 203                ;inputg syscall id
+mov eax, XANIN_INPUTG                ;inputg syscall id
 ESP_GET_NTH_ARGUMENT ecx, 1
 XANIN_INVOKE_SYSTEM_CALL
 ret
 
 __sys_keyinfo_get:          ;void __sys_keyinfo_get(key_info_t* ptr)
-mov eax, 204            ;keyinfo_get syscall id
+mov eax, XANIN_KEYINFO_GET  ;keyinfo_get syscall id
 ESP_GET_NTH_ARGUMENT ecx, 1
 XANIN_INVOKE_SYSTEM_CALL
 ret
+
+__sys_input_add_object_to_observe:
+mov eax, XANIN_INPUT_ADD_OBJECT_TO_OBSERVE
+ESP_GET_NTH_ARGUMENT ecx, 1
+ESP_GET_NTH_ARGUMENT edx, 2
+XANIN_INVOKE_SYSTEM_CALL
+ret
+
+__sys_input_remove_object_from_observe:
+mov eax, XANIN_INPUT_REMOVE_OBJECT_FROM_OBSERVE
+ESP_GET_NTH_ARGUMENT ecx, 1
+XANIN_INVOKE_SYSTEM_CALL
+ret
+
+__sys_input_handle_observed_objects:
+mov eax, XANIN_INPUT_HANDLE_OBSERVED_OBJECTS
+ESP_GET_NTH_ARGUMENT ecx, 1
+XANIN_INVOKE_SYSTEM_CALL
+ret
+
+__sys_input_add_handler:
+ESP_GET_NTH_ARGUMENT ecx, 1
+mov eax, XANIN_INPUT_ADD_HANDLER
+XANIN_INVOKE_SYSTEM_CALL
+ret
+
+__sys_input_remove_handler:
+ESP_GET_NTH_ARGUMENT ecx, 1
+mov eax, XANIN_INPUT_REMOVE_HANDLER
+XANIN_INVOKE_SYSTEM_CALL
+ret
+
+__sys_input_remove_user_handlers:
+mov eax, XANIN_INPUT_REMOVE_USER_HANDLERS
+XANIN_INVOKE_SYSTEM_CALL
+ret
+
+__sys_input_call_handlers:
+mov eax, XANIN_INPUT_CALL_HANDLERS
+ESP_GET_NTH_ARGUMENT ecx, 1
+XANIN_INVOKE_SYSTEM_CALL
+ret
+
+
