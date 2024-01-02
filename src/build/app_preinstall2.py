d61deec6d67570ab41b5c8e5f326d5fea5cf5f58
Witut2005 <wiktor.glogowski@onet.pl>
2024-01-02 21:16:54 +0100

    get bcd current time and date

--- src/build/app_preinstall2.py
+++ src/build/app_preinstall2.py
@@ -4,17 +4,8 @@ from colorama import Fore
 from colorama import Style
 import sys
 import os 
+from datetime import datetime
 from macros import * 
-
-class PreinstallableObject: 
-    def __init__(self, name, type, first_sector, sectors_occupied):
-        self.name = name
-        self.type = type
-        self.first_sector = first_sector
-        self.sectors_occupied = sectors_occupied
-    
-    def display_information(self):
-        print(f"name: {self.name}\ntype: {self.type}\nfirst_sector: {self.first_sector}\nentry_size: {self.sectors_occupied}\n")
 
 
 args = argparse.ArgumentParser()
@@ -24,7 +15,27 @@ args.add_argument('--image', action='store', type=str, required=True)
 
 args = args.parse_args()
 
-def align_to_size(file, size):
+def decimal_to_bcd(number):
+    ascii_digits = number.encode('ascii')
+    bcd_digits = b''
+    for i in range(len(ascii_digits)):
+        bcd_digits += (ascii_digits[i] - 48).to_bytes(1, byteorder='little')
+
+    return bcd_digits
+
+def write_xin_entry_to_file(file, entry):
+    return
+
+def convert_string_to_aligned_bytes(input_string, alignment_size):
+    encoded_bytes = input_string.encode('ascii')
+    padding_size = alignment_size - len(encoded_bytes) % alignment_size
+
+    aligned_bytes = encoded_bytes + b'\0' * padding_size
+
+    return aligned_bytes
+
+
+def align_file_to_size(file, size):
     file_size = os.path.getsize(file.name)
     print(f"file name: {file.name}")
     print(f"file size: {file_size}")
@@ -37,33 +48,76 @@ def align_to_size(file, size):
     file.write(bytes([0] * (size - tmp)))
     print('padded ', size - tmp, ' bytes')
 
+# class PreinstallableObject: 
+#     def __init__(self, name, type, first_sector, sectors_occupied):
+#         self.name = name
+#         self.type = type
+#         self.first_sector = first_sector
+#         self.sectors_occupied = sectors_occupied
+    
+#     def display_information(self):
+#         print(f"name: {self.name}\ntype: {self.type}\nfirst_sector: {self.first_sector}\nentry_size: {self.sectors_occupied}\n")
+
+xin_current_date = decimal_to_bcd(datetime.now().strftime('%d%m%Y'))
+xin_current_time = decimal_to_bcd(datetime.now().strftime('%H%M'))
+
+print("NORMAL: ", datetime.now().strftime('%d%m%Y'))
+# print("BCD", xin_current_time[0])
+    
+class XinEntryData: 
+    def __init__(self, path, type, permissions, size=0, first_sector=0):
+        self.path = convert_string_to_aligned_bytes(path, XIN_MAX_ENTRY_PATH_LENGTH)
+        self.type = type
+        self.creation_date = xin_current_date 
+        self.creation_time = xin_current_time 
+        self.modification_date = self.creation_date
+        self.modification_time = self.creation_time
+        self.permissions = permissions
+        
+        if(self.type != XIN_DIRECTORY):
+            self.size = size.to_bytes(4, byteorder='little')
+            self.first_sector = size.to_bytes(4, byteorder='little')
+
+    def write(self, file):
+        file.write(self.path)
+        file.write(self.type)
+        file.write(self.creation_date)
+        file.write(self.creation_tiime)
+        file.write(self.modification_date)
+        file.write(self.modification_tiime)
+        file.write(self.size)
+        file.write(self.first_sector)
+        file.write(int(0).to_bytes(4, 'little')) #File Info ptr
+
+
+
 def preinstall(file): 
 
     xin_pointers_begin = os.path.getsize(file.name)
     xin_entries_begin = xin_pointers_begin + (SECTOR_SIZE * 512)
+    xin_preinstalled_objects_cursor = xin_entries_begin + (SECTOR_SIZE * 256) #current preintall objects data file cursor
+
     xin_entries_current_entry_index = 0
     # xin_filesystem_pointers = xin_filesystem_pointers_begin 
 
     xanin_entries_to_preinstall = [
-        PreinstallableObject('/', XIN_DIRECTORY, 0, 0),
-        PreinstallableObject('/file_system.bin', XIN_DIRECTORY, 0, 0),
-        PreinstallableObject('/enter_real_mode.bin', XIN_FILE, 0x12, 20),
-        PreinstallableObject('/boot.bin', XIN_FILE, 1, 1),
-        PreinstallableObject('/shutdown.bin', XIN_FILE, 1, 2),
-        PreinstallableObject('/fast_real_mode_enter.bin', XIN_FILE, 5, 1), 
-        PreinstallableObject('/fast_real_mode_return.bin', XIN_FILE, 6, 1),
-        PreinstallableObject('/screenshot/', XIN_DIRECTORY, 0, 0),
-        PreinstallableObject('/ivt', XIN_FILE, 1000, 2),
-        # ['/',                           XIN_DIRECTORY, 0, 0],
-        # ['/file_system.bin',            XIN_FILE, 0x12, 20],
-        # ['/enter_real_mode.bin',        XIN_FILE, 1, 1],
-        # ['/boot.bin',                   XIN_FILE, 0, 1],
-        # ['/shutdown.bin',               XIN_FILE, 1, 2],
-        # ['/fast_real_mode_enter.bin',   XIN_FILE, 5, 1],
-        # ['/fast_read_mode_return.bin',  XIN_FILE, 6, 1],
-        # ['/screenshot/',                XIN_DIRECTORY, 0, 0], 
-        # ['/ivt',                        XIN_FILE, 1000, 2]
+
+        # XinEntryData('/', XIN_DIRECTORY, XIN_MAX_PERMISSIONS, )
+
+        # PreinstallableObject('/', XIN_DIRECTORY, 0, 0),
+        # PreinstallableObject('/file_system.bin', XIN_DIRECTORY, 0, 0),
+        # PreinstallableObject('/enter_real_mode.bin', XIN_FILE, 0x12, 20),
+        # PreinstallableObject('/boot.bin', XIN_FILE, 1, 1),
+        # PreinstallableObject('/shutdown.bin', XIN_FILE, 1, 2),
+        # PreinstallableObject('/fast_real_mode_enter.bin', XIN_FILE, 5, 1), 
+        # PreinstallableObject('/fast_real_mode_return.bin', XIN_FILE, 6, 1),
+        # PreinstallableObject('/screenshot/', XIN_DIRECTORY, 0, 0),
+        # PreinstallableObject('/ivt', XIN_FILE, 1000, 2),
+
+
     ]
+
+    file.seek(xin_entries_begin)
 
     for entry in xanin_entries_to_preinstall:
 
@@ -73,10 +127,9 @@ def preinstall(file):
                 file.write(XIN_ALLOCATED)
             file.write(XIN_EOF)
 
-        xin_entries_cursor = xin_entries_begin + (xin_entries_current_entry_index * XIN_ENTRY_SIZE)
+        # xin_entries_cursor = xin_entries_begin + (xin_entries_current_entry_index * XIN_ENTRY_SIZE)
 
-        file.seek()
-        file.write(bytes(entry.name, 'ascii'))
+        file.write(bytes(entry.name, 'ascii', ))
         file.seek(xin_entries_cursor + 38) #entry path
         file.write(bytes(entry.name, 'ascii'))
         file.write(bytes(13))
@@ -86,8 +139,7 @@ def preinstall(file):
 
     # file.close()
 
-    xin_filesystem_pointers = int(SECTOR_SIZE * 0x12 + (SECTOR_SIZE * 3)) # must be higher the begin
-    print('EXTERNAL APPS LOCATION: ', hex(xin_filesystem_entries), ' ', hex(xin_filesystem_pointers))
+    print(f"EXTERNAL APPS LOCATION: {hex(xin_pointers_begin)} {hex(xin_preinstall_objects_data)}")
 
     directories = set() 
 
@@ -120,11 +172,7 @@ def preinstall(file):
 
         data = aha.read()
         
-        if(xin_filesystem_pointers == 0):
-            print('ABORT')
-
-        
-        file.seek(xin_filesystem_pointers)
+        file.seek(xin_preinstalled_objects_cursor)
         x = 0
         file_lenght = int(len(data) / SECTOR_SIZE)
         if(len(data) % 512 != 0):
@@ -140,7 +188,7 @@ def preinstall(file):
 
         file.write(XIN_EOF)
         
-        file.seek(xin_filesystem_entries)
+        file.seek(xin_entries_begin)
         file.write(bytes('/' + current_file, 'ascii'))
         file.seek(xin_filesystem_entries + 38) #entry path
         file.write(bytes(XIN_FILE, 'ascii'))
@@ -173,10 +221,12 @@ def preinstall(file):
         xin_filesystem_entries += XIN_ENTRY_SIZE
 
 def main(args):
-    print(os.path.abspath(args.image))
-    args.image = os.path.abspath(args.image)
-    image = open(args.image, 'rb+')
-    align_to_size(image, 16)
+    print(datetime.now().strftime('%d%m%Y'))
+    print(datetime.now().strftime('%H%M'))
+    # print(os.path.abspath(args.image))
+    # args.image = os.path.abspath(args.image)
+    # image = open(args.image, 'rb+')
+    # align_to_size(image, 16)
     # print(image.read()[0])
 
 if __name__ == '__main__':
